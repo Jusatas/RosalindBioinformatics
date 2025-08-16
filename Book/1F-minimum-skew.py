1f981ce3349ec27aba2bb7fd171b1c48525686d3
justas <justas.draksas@gmail.com>
2025-08-16 18:15:48 +0300

    Solve 1F

--- Book/1F-minimum-skew.py
+++ Book/1F-minimum-skew.py
@@ -3,32 +3,48 @@
 # Takes in a string Genome and forms a an array Skew
 # Skew contains the difference between the total number
 # of 'G' and 'C' found so far for each position of Skew
-# Returns the indexes where Skew has the minimum value
+# Returns the indices where Skew has the minimum value
 # These values are potential OriC sites.
 
 import argparse
 from rosalind_utils import read_input
 
 
+def find_skew(genome):
+    """Takes in a string genome, returns a skew array
+    and and array containing it's minimum value indices"""
+    skew = [0]
+    min_value_indices = []
+    min_value = 0
+
+    for i, nucleotide in enumerate(genome):
+        if nucleotide == "C":
+            skew.append(skew[-1] - 1)
+        elif nucleotide == "G":
+            skew.append(skew[-1] + 1)
+        else:
+            skew.append(skew[-1])
+
+        if skew[-1] < min_value:
+            min_value = skew[-1]
+            min_value_indices[:] = [i + 1]
+        elif skew[-1] == min_value:
+            min_value_indices.append(i + 1)
+
+    return skew, min_value_indices
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Return all distinct k-mers forming clumps")
 
-    parser.add_argument("text", help="Full text string or file")
-    parser.add_argument("kmer_length", help="How long the k-mers should be")
-    parser.add_argument("window_size", help="How big the window should be")
-    parser.add_argument("kmer_count", help="How many kmers for a clump")
-
+    parser.add_argument(
+            "genome",
+            help="String containing string repesenting a genome")
     args = parser.parse_args()
 
-    text = read_input(args.text)
-    kmer_length = int(read_input(args.kmer_length))
-    window = int(read_input(args.window_size))
-    kmer_count = int(read_input(args.kmer_count))
-
-    result = find_frequent_patterns_clumping(
-            text, kmer_length, window, kmer_count)
+    genome = read_input(args.genome)
+    result = (find_skew(genome)[1])
     print(" ".join(map(str, result)))
 
 
